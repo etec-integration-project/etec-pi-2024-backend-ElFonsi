d8936b5ebdb79ee3c3129dac1b1073ce5fd04ae2
ElFonsi <a.magallanes@alumno.etec.um.edu.ar>
2024-11-08 12:06:26 -0300

    Spritn 8 commit 16

--- Computer_shop_backend/src/server.ts
+++ Computer_shop_backend/src/server.ts
@@ -10,9 +10,20 @@ const app = express()
 
 app.use(morgan('dev'))
 
+const allowedOrigins = [process.env.CORS_ORIGIN];
+
 app.use(cors({
-    origin: process.env.CORS_ORIGIN  
+    origin: function (origin, callback) {
+      if (!origin || allowedOrigins.includes(origin)) {
+        callback(null, true);
+      } else {
+        callback(new Error('Origen no permitido por CORS'));
+      }
+    },
+    methods: ['GET', 'POST', 'DELETE', 'OPTIONS'],
+    allowedHeaders: ['Content-Type', 'Authorization'],
   }));
+
   
 app.use(express.json())
 
