bbbf8c70173f8d0aff774089b1e91292d379d7d8
ElFonsi <a.magallanes@alumno.etec.um.edu.ar>
2024-11-15 09:08:50 -0300

    Sprint 8 final

--- Computer_shop_backend/src/db.ts
+++ Computer_shop_backend/src/db.ts
@@ -5,33 +5,33 @@ import Carrito from "./models/Carrito";
 import {config} from "dotenv"
 config()
 
-// export const AppDataSource= new DataSource({
-//     type: "mysql",
-//     host:process.env.MYSQLDB_HOST,
-//     port:+(process.env.MYSQLDB_DOCKER_PORT!),
-//     username:process.env.MYSQLDB_USERNAME,
-//     password:process.env.MYSQLDB_PASSWORD,
-//     database:process.env.MYSQLDB_DATABASE,
-//     synchronize:true,
-//     logging:true,
-//     entities:[Producto, Usuario, Carrito],
-//     subscribers:[],
-//     migrations:[]
-// })
-
 export const AppDataSource= new DataSource({
     type: "mysql",
-    host:'127.0.0.1',
-    port:3306,
-    username:'root',
-    password:'1234',
-    database:'db',
+    host:process.env.MYSQLDB_HOST,
+    port:+(process.env.MYSQLDB_DOCKER_PORT!),
+    username:process.env.MYSQLDB_USERNAME,
+    password:process.env.MYSQLDB_PASSWORD,
+    database:process.env.MYSQLDB_DATABASE,
     synchronize:true,
     logging:true,
     entities:[Producto, Usuario, Carrito],
     subscribers:[],
     migrations:[]
 })
+
+// export const AppDataSource= new DataSource({
+//     type: "mysql",
+//     host:'127.0.0.1',
+//     port:3306,
+//     username:'root',
+//     password:'1234',
+//     database:'db',
+//     synchronize:true,
+//     logging:true,
+//     entities:[Producto, Usuario, Carrito],
+//     subscribers:[],
+//     migrations:[]
+// })
 
 
 
